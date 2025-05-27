31a23d6fd134b10d2b26f4e368326d69c18b6b4b
AlexandreMoraes12 <109194555+AlexandreMoraes12@users.noreply.github.com>
2025-05-26 22:24:38 -0300

    Update credential.rb

--- features/support/credential.rb
+++ features/support/credential.rb
@@ -2,8 +2,8 @@
 
 CREDENTIAL = {
     user:{
-        email: 'alexandre.furtado.20430@aluno.facape.br',
-        password: '88161658'
+        email: '',
+        password: ''
     },
     user_2:{
         email: 'email_dois@email.com',
